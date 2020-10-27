ada74659a6c069d29d20e92c8ceaa85eca0e5fc6
Javier Ruiz Trejo <jruizt850@gmail.com>
2020-10-27 07:56:47 -0600

    Modificacion de rutas del proyecto

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
-Auth::routes();
+/*Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');*/

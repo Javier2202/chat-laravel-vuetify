dc20afc971da4502c3e43adcd7dcda64146be462
Javier Ruiz Trejo <jruizt850@gmail.com>
2020-10-27 07:58:56 -0600

    Registro de componente login-component

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,6 +23,7 @@ Vue.use(Vuetify)
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('login-component',require('./components/LoginComponent.vue').default);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('conversaciones', require('./components/ConversacionesComponent.vue').default);
 

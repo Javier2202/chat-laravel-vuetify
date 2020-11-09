f02b686278219c5b7aeb71813f88583b5e526876
Javier Ruiz Trejo <jruizt850@gmail.com>
2020-11-09 07:50:57 -0600

    Migracion de la tabla imagenes

---
+++ database/migrations/2020_11_08_122206_create_imagens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagenes', function (Blueprint $table) {
+            $table->id();
+            $table->text('url');
+            $table->string('nombre');
+            $table->string('tipo',4);
+            $table->string('carpeta');
+            $table->morphs('imageneable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imagens');
+    }
+}

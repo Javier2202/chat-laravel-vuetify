f33fc702d2c2ee30bfddc9de1d7fbc7ea08718ec
Javier Ruiz Trejo <jruizt850@gmail.com>
2020-11-11 05:58:20 -0600

    Cambios visuales de la vista

--- resources/js/components/RegistroComponent.vue
+++ resources/js/components/RegistroComponent.vue
@@ -1,6 +1,43 @@
 <template>
     <div>
         <h1>Registro</h1>
+        <v-app>
+            <!-- Sizes your content based upon application components -->
+            <v-main>
+                <v-container>
+                    <v-form>
+                        <v-file-input
+                            accept="image/png, image/jpeg, image/jpg,"
+                            show-size
+                            placeholder="Imagen"
+                            prepend-icon="mdi-camera"
+                            label="Perfil"
+                        ></v-file-input>
+                        <v-text-field
+                            label="Nombre completo"
+                            required>
+                        </v-text-field>
+                        <v-text-field
+                            label="Correo electronico"
+                            required>
+                        </v-text-field>
+                        <v-btn
+                            color="green">
+                            Registrar
+                        </v-btn>
+                        <v-btn
+                            color="orange">
+                            Limpiar
+                        </v-btn>
+                        <v-btn
+                            color="red"
+                            :to="{name:'home'}">
+                            Cancelar
+                        </v-btn>
+                    </v-form>
+                </v-container>
+            </v-main>
+        </v-app>        
     </div>
 </template>
 

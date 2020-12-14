d6965c910356b8ef302c0d7b69e8a1e50a5d6665
Javier Ruiz Trejo <jruizt850@gmail.com>
2020-12-14 14:06:20 -0600

    Listado de conversaciones de un usuario autenticado

--- resources/js/components/ConversacionesComponent.vue
+++ resources/js/components/ConversacionesComponent.vue
@@ -1,11 +1,64 @@
 <template>
     <div>
         Conversaciones
+        <v-btn
+            text
+            text-center
+            v-on:click="logout"
+            >Cerrar sesión
+        </v-btn>
     </div>
 </template>
 
 <script>
     export default {
-        
+        mounted() {
+            if(localStorage.getItem("authToken") === null){
+                this.$router.push("/");
+            }
+            this.getConversaciones();
+        },
+        data:function(){
+        	return{
+        		conversaciones: [],
+        	}
+        },
+        methods: {
+            logout:function(){
+                if(localStorage.getItem("authToken") !== null){
+                    var temp_this = this;
+                    axios({
+                        method: 'get',
+                        url: 'api/v1/auth/logout',
+                        headers: {
+                            Authorization: 'Bearer ' + localStorage.getItem("authToken"),
+                        }
+                    }).then(function (response) {
+                        if(response.status == 200){
+                            localStorage.clear();
+                        }
+                        console.log(response);
+                    }).catch(function (error) {
+                        console.log(error);
+                    }).then(function(){
+                        temp_this.$router.go(-1);
+                    });
+                }
+            },
+            getConversaciones:function(){
+                var temp_this = this;
+                axios({
+                    method: 'get',
+                    url: 'api/v1/conversacion',
+                    headers: {
+                        Authorization: 'Bearer ' + localStorage.getItem("authToken"),
+                    }
+                }).then(function (response){
+                    temp_this.conversaciones = response.data.data;
+                }).catch(function(error){
+                    console.log(error);
+                });
+            }
+        } 
     }
 </script>

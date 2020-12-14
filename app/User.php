9737901c3fadc314e4d9e3f00c52f4684a86b4d0
Javier Ruiz Trejo <jruizt850@gmail.com>
2020-12-14 13:59:34 -0600

    Comentar linea de incluir relación de imagen

--- app/User.php
+++ app/User.php
@@ -11,7 +11,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
 
-    protected $with = ['imagen'];
+    //protected $with = ['imagen'];
 
     /**
      * The attributes that are mass assignable.
